0674c3966c5d24026af42fb007568a32d828b74b
nacb654 <nacb654@gmail.com>
2025-12-03 10:20:37 -0500

    quitando front

--- frontend/app/(auth)/layout.js
+++
@@ -1,7 +0,0 @@
-export default function AuthLayout({ children }) {
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-primary-50 via-background to-primary-100">
-      {children}
-    </div>
-  );
-}
