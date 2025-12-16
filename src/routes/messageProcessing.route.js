520d212ff88949582c639690bd2ff6329e8d0542
Diego <111472703+Kitaro1@users.noreply.github.com>
2025-12-16 11:58:56 -0500

    Quito el validador

--- src/routes/messageProcessing.route.js
+++ src/routes/messageProcessing.route.js
@@ -1,15 +1,11 @@
 const { Router } = require("express");
 
-const { 
-    processMessageValidator
- } = require("../validators/messageProcessing.validators.js");
 const handleValidationErrors = require("../middlewares/handleValidation.middleware.js");
 const MessageProcessingController = require("../controllers/messageProcessing.controller.js");
 
 const router = Router();
 
 router.post("/message", 
-    processMessageValidator, 
     handleValidationErrors, 
     MessageProcessingController.processMessage
 );
