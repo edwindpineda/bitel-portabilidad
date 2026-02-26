e0f7f6af3e9ba94f1b9e41b6424ea0f2304ec03a
Nicolas Castro <140445183+NACB654@users.noreply.github.com>
2026-02-26 10:01:02 -0500

    Update persona.route.js

--- src/routes/crm/persona.route.js
+++ src/routes/crm/persona.route.js
@@ -3,9 +3,9 @@ const PersonaController = require("../../controllers/crm/persona.controller.js")
 
 const router = Router();
 
-router.get("/:id", PersonaController.getById);
-router.post("/", PersonaController.create);
-router.put("/:id", PersonaController.update);
-router.post("/bulk-assign", PersonaController.bulkAssign);
+router.get("/persona/:id", PersonaController.getById);
+router.post("/persona", PersonaController.create);
+router.put("/persona/:id", PersonaController.update);
+router.post("/persona/bulk-assign", PersonaController.bulkAssign);
 
 module.exports = router;
