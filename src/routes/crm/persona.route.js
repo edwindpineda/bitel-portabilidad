b33f3b68f8d12933ba9683641e56337fda3fd1f0
nacb654 <nacb654@gmail.com>
2026-03-02 17:12:12 -0500

    fix en rutas persona

--- src/routes/crm/persona.route.js
+++ src/routes/crm/persona.route.js
@@ -3,10 +3,10 @@ const PersonaController = require("../../controllers/crm/persona.controller.js")
 
 const router = Router();
 
-router.get("/", PersonaController.listAll);
-router.get("/:id", PersonaController.getById);
-router.post("/", PersonaController.create);
-router.put("/:id", PersonaController.update);
-router.post("/bulk-assign", PersonaController.bulkAssign);
+router.get("/persona", PersonaController.listAll);
+router.get("/persona/:id", PersonaController.getById);
+router.post("/persona", PersonaController.create);
+router.put("/persona/:id", PersonaController.update);
+router.post("/persona/bulk-assign", PersonaController.bulkAssign);
 router.get("/persona/celular/:celular", PersonaController.searchByCelular);
 module.exports = router;
